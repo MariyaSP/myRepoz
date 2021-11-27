8acbf26ed4c85ae9dafe5590f5c6385ff78db835
Mariya Kartseva <kjojoj@rambler.ru>
2021-11-27 16:28:40 +0300

    переменные, работа со строками

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
-let title,
-screens,
-screenPrice,
-rollback,
-fullPrice,
-adaptive ;
+let title = "калькулятор верстки",
+    screens = "Простые, Сложные, Интерактивные",
+    screenPrice = 20000,
+    rollback = 20,
+    fullPrice = 100000,
+    adaptive = true;
 
 
-console.log("Hello, JS");
-alert("Hello, JS");''
+console.log("title " + typeof title, ", fullPrice " + typeof fullPrice, ", adaptive " + typeof adaptive);
+console.log(screens.length);
+
+console.log("Стоимость верстки экранов " + screenPrice + " рублей/долларов/гривен/юани");
+console.log("Стоимость разработки сайта " + fullPrice + " рублей/ долларов/гривен/юани");
+
+console.log(screens.toLocaleLowerCase().split(", "));
+console.log("Процент отката посреднику за работу:" + fullPrice * (rollback/100));
