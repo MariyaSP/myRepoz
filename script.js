65e0b38fb015fabd57926b8986f4a9ed5a5c26f2
Mariya Kartseva <kjojoj@rambler.ru>
2021-11-29 23:17:39 +0300

    feat(scrip.js): new feat

--- script.js
+++ script.js
@@ -1,16 +1,44 @@
-let title = "калькулятор верстки",
-    screens = "Простые, Сложные, Интерактивные",
-    screenPrice = 20000,
-    rollback = 20,
-    fullPrice = 100000,
-    adaptive = true;
+let rollback = 20;
 
 
-console.log("title " + typeof title, ", fullPrice " + typeof fullPrice, ", adaptive " + typeof adaptive);
-console.log(screens.length);
+const title = prompt("Как называется ваш проект?");
 
-console.log("Стоимость верстки экранов " + screenPrice + " рублей/долларов/гривен/юани");
-console.log("Стоимость разработки сайта " + fullPrice + " рублей/ долларов/гривен/юани");
+const screens = prompt("Какие типы экранов нужно разработать?");
+const screenPrice = +prompt("Сколько будет стоить такая работа?");
+const adaptive = prompt("Нужен ли адаптив на сайте?");
+const service1 = prompt("Какой дополнительный тип услуги нужен?");
+const servicePrice1 = +prompt("Сколько это будет стоить?")
+const service2 = prompt("Какой дополнительный тип услуги нужен?");
+const servicePrice2 = +prompt("Сколько это будет стоить?")
 
-console.log(screens.toLocaleLowerCase().split(", "));
-console.log("Процент отката посреднику за работу:" + fullPrice * (rollback/100));
+
+const fullPrice = screenPrice + servicePrice1 + servicePrice2;
+
+const servicePercentPrice = Math.ceil(fullPrice - fullPrice * (rollback / 100));
+console.log("Стоимость проекта за вычетом отката: " + servicePercentPrice);
+
+
+switch (true) {
+    case fullPrice >= 30000:
+        console.log("Даем скидку в 10%");
+        break;
+    case fullPrice >= 15000 && fullPrice < 30000:
+        console.log("Даем скидку в 5%");
+        break;
+    case fullPrice >= 0 && fullPrice < 15000:
+        console.log("Скидка не предусмотрена");
+        break;
+    case fullPrice<0:
+        console.log("Что-то пошло не так");
+        break;        
+}
+
+
+// console.log("title " + typeof title, ", fullPrice " + typeof fullPrice, ", adaptive " + typeof adaptive);
+// console.log(screens.length);
+
+// console.log("Стоимость верстки экранов " + screenPrice + " рублей");
+// console.log("Стоимость разработки сайта " + fullPrice + " рублей");
+
+// console.log(screens.toLocaleLowerCase().split(", "));
+// console.log("Процент отката посреднику за работу:" + fullPrice * (rollback / 100));
